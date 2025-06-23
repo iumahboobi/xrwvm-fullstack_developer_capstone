1f821705b663783b316016385fe219afb59580a6
iumahboobi <iumahboobi@gmail.com>
2025-06-23 13:03:37 -0400

    flake8 file added

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -97,6 +97,11 @@ def initiate():
             "car_make": car_make_instances[4],
         },
     ]
-    
+
     for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'],type =data['type'], year = data['year'])
+        CarModel.objects.create(
+            name=data["name"],
+            car_make=data["car_make"],
+            type=data["type"],
+            year=data["year"],
+        )
