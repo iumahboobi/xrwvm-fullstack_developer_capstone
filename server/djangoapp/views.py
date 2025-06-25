7b1dc1ded721e47f5371eecf2c7f1b7934b380b3
iumahboobi <iumahboobi@gmail.com>
2025-06-25 06:36:42 -0400

    lintcode issue solved

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -155,7 +155,7 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
